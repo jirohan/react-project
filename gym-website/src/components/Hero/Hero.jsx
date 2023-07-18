2e9be6e3e5f765ec7aa2e70d022ee39f1dcca245
Rohan Maharjan <hondazmr6@gmail.com>
2023-07-18 17:49:25 +0545

    final hero

--- gym-website/src/components/Hero/Hero.jsx
+++ gym-website/src/components/Hero/Hero.jsx
@@ -1,6 +1,10 @@
 import React from "react";
 import "./Hero.css";
 import Header from "../Header/Header";
+import hero_image from "../../assets/hero_image.png";
+import hero_image_back from "../../assets/hero_image_back.png";
+import Heart from "../../assets/heart.png";
+import Calories from "../../assets/calories.png";
 
 const Hero = () => {
   return (
@@ -43,8 +47,16 @@ const Hero = () => {
             <span>Fitenss programs</span>
           </div>
         </div>
+
+        {/* hero buttons */}
+        <div className="hero-buttons">
+            <buttons className="btn">Get Started</buttons>
+            <buttons className="btn">Learn More</buttons>
+        </div>
       </div>
-      <div className="right-h">right-side</div>
+      <div className="right-h">
+        
+      </div>
     </div>
   );
 };
