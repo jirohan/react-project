b99e964ce8104ecf77e9282840305d93c021194e
Rohan Maharjan <hondazmr6@gmail.com>
2023-07-20 16:22:57 +0545

    navbar completed

---
+++ data-website/src/components/Hero.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <div className='text-white'>
+        <div className='max-w-[800px] mt-[-96px] w-full h-screen mx-auto text-center flex flex-col justify-center'>
+            <p className='text-[#00df9a] uppercase font-bold p-2'>Growing with data analytics</p>
+            <h1 className='md:text-7xl sm:text-6xl text-4xl font-bold md:py-6'>Grow with data</h1>
+            <div>
+                <p className='md:tex-5xl sm:text-4xl text-xl font-bold'>Fast, FLexible, Financing for </p>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Hero
