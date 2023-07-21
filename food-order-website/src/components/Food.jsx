e960324fb8c6b8abcac7caed11bc7687478e69b5
Rohan Maharjan <hondazmr6@gmail.com>
2023-07-22 00:21:47 +0545

    filter completed

--- food-order-website/src/components/Food.jsx
+++ food-order-website/src/components/Food.jsx
@@ -4,6 +4,25 @@ import {data} from '../data/data.js'
 const Food = () => {
     const [foods, setFoods] = useState(data);
 
+    //filter type burgers/pizza
+    const filterType = (category) => {
+        setFoods(
+            data.filter((item) => {
+                return item.category === category;
+            })
+        );
+    };
+
+    //filter by price
+    const filterPrice = (price) => {
+        setFoods(
+            data.filter((item) => {
+                return item.price === price;
+            })
+        );
+    };
+
+
   return (
     <div className='max-w-[1360px] m-auto px-4 py-12'>
         <h1 className='text-orange-600 font-bold text-4xl text-center'>Top Rated Menu Items</h1>
@@ -15,11 +34,11 @@ const Food = () => {
             <div>
                 <p className='font-bold text-gray-700'>Filter Type</p>
                 <div className='flex justify-between flex-wrap'>
-                    <button className='m-1 border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white '>All</button>
-                    <button className='m-1 border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white '>Burgers</button>
-                    <button className='m-1 border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white '>Pizza</button>
-                    <button className='m-1 border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white '>Salads</button>
-                    <button className='m-1 border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white '>Chicken</button>
+                    <button onClick={()=> setFoods(data)} className='m-1 border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white '>All</button>
+                    <button onClick={()=> filterType('burger')} className='m-1 border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white '>Burgers</button>
+                    <button onClick={()=> filterType('pizza')} className='m-1 border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white '>Pizza</button>
+                    <button onClick={()=> filterType('salads')} className='m-1 border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white '>Salads</button>
+                    <button onClick={()=> filterType('chicken')} className='m-1 border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white '>Chicken</button>
                 </div>
             </div>
 
@@ -27,10 +46,10 @@ const Food = () => {
             <div>
                 <p className='font-bold text-gray-700'>Filter Price</p>
                 <div className='flex justify-between max-w-[390px] w-full'>
-                    <button className='m-1 border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white '>$</button>
-                    <button className='m-1 border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white '>$$</button>
-                    <button className='m-1 border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white '>$$$</button>
-                    <button className='m-1 border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white '>$$$$</button>
+                    <button onClick={()=> filterPrice('$')} className='m-1 border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white '>$</button>
+                    <button onClick={()=> filterPrice('$$')} className='m-1 border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white '>$$</button>
+                    <button onClick={()=> filterPrice('$$$')} className='m-1 border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white '>$$$</button>
+                    <button onClick={()=> filterPrice('$$$$')} className='m-1 border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white '>$$$$</button>
                 </div>
             </div>
         </div>
