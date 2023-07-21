4e40c9d4cd28ea43155d6724d3361ade43f5d423
Rohan Maharjan <hondazmr6@gmail.com>
2023-07-21 13:45:35 +0545

    cards completed

--- data-website/src/App.jsx
+++ data-website/src/App.jsx
@@ -3,6 +3,7 @@ import Navbar from './components/Navbar'
 import Hero from './components/Hero'
 import Analytics from './components/Analytics'
 import Newsletter from './components/Newsletter'
+import Cards from './components/Cards'
 
 const App = () => {
   return (
@@ -11,6 +12,7 @@ const App = () => {
       <Hero />
       <Analytics />
       <Newsletter />
+      <Cards />
     </div>
   )
 }
