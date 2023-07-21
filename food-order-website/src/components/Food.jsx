2359205e6d50d7c6fabfe5a6ed2af8a403821d46
Rohan Maharjan <hondazmr6@gmail.com>
2023-07-22 00:10:09 +0545

    maping completed

--- food-order-website/src/components/Food.jsx
+++ food-order-website/src/components/Food.jsx
@@ -40,10 +40,10 @@ const Food = () => {
             {foods.map((item, index)=> (
                 <div key={index} className='border shadow-lg rounded-lg hover:scale-105 duration-300'>
                     <img src={item.image} alt={item.name} className='w-full h-[200px] object-cover rounded-t-lg' />
-                    <div>
-                        <p>{item.name}</p>
+                    <div className='flex justify-between px-2 py-4'>
+                        <p className='font-bold'>{item.name}</p>
                         <p>
-                            <span>{item.price}</span>
+                            <span className='bg-orange-500 text-white p-1 rounded-full'>{item.price}</span>
                         </p>
                     </div>
                 </div>
