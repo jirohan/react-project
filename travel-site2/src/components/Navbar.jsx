5cd4f6da371f6ed29f1489a6e9549eb93141a4f0
Rohan Maharjan <hondazmr6@gmail.com>
2023-07-24 13:50:14 +0545

    navbar completed

--- travel-site2/src/components/Navbar.jsx
+++ travel-site2/src/components/Navbar.jsx
@@ -1,22 +1,38 @@
-import React from 'react'
-import {HiMenuAlt3} from 'react-icons/hi'
+import React, { useState } from 'react';
+import { HiMenuAlt3 } from 'react-icons/hi';
 
 const Navbar = () => {
+  const [nav, setNav] = useState(false); // Change "cont" to "const"
+  const handleNav = () => {
+    setNav(!nav);
+    if(!nav){
+      document.body.style.overflow = 'hidden'
+    } else{
+      document.body.style.overflow = 'scroll'
+    }
+  };
+
   return (
     <div className='absolute w-full flex justify-between p-4 items-center'>
-        <h1 className='text-white font-bold text-2xl z-20'>VACATIONS</h1>
-        <HiMenuAlt3 className='text-white cursor-pointer z-20' size={25} />
-        <div className='fixed text-gray-300 left-0 top-0 w-full h-screen bg-black/90 px-4 py-7 flex-col z-10'>
-            <ul>
-                <li>Home</li>
-                <li>Destinations</li>
-                <li>Reservations</li>
-                <li>Amenities</li>
-                <li>Rooms</li>
-            </ul>
-        </div>
+      <h1 className='text-white font-bold text-2xl z-20'>VACATIONS</h1>
+      <HiMenuAlt3 onClick={handleNav} className='text-white cursor-pointer z-20' size={35} />
+      <div
+        className={
+          nav
+            ? 'ease-in duration-300 fixed text-gray-300 left-0 top-0 w-full h-screen bg-black/90 px-4 py-7 flex-col z-10'
+            : 'absolute top-0 h-screen left-[-100%] ease-in duration-500'
+        }
+      >
+        <ul className='flex flex-col w-full h-full items-center justify-center'>
+          <li className='font-bold text-3xl p-8'>Home</li>
+          <li className='font-bold text-3xl p-8'>Destinations</li>
+          <li className='font-bold text-3xl p-8'>Reservations</li>
+          <li className='font-bold text-3xl p-8'>Amenities</li>
+          <li className='font-bold text-3xl p-8'>Rooms</li>
+        </ul>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
