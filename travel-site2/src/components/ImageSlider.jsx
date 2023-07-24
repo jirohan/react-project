b948580a4f93479ade755b7bd7a7ba853f32a3a9
Rohan Maharjan <hondazmr6@gmail.com>
2023-07-24 16:36:33 +0545

    slider completed

--- travel-site2/src/components/ImageSlider.jsx
+++ travel-site2/src/components/ImageSlider.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import {BsChevronCompactLeft, BsChevronCompactRight} from 'react-icons/bs';
+import {RxDotFilled} from 'react-icons/rx'
 
 const ImageSlider = () => {
     const slides = [
@@ -34,6 +35,10 @@ const ImageSlider = () => {
         setCurrentIndex(newIndex);
     }
 
+    const goToSlide = (slideIndex) => {
+        setCurrentIndex(slideIndex)
+    }
+
   return (
     <div className='max-w-[1400px] h-[580px] w-full m-auto py-16 px-4 relative group'>
         <div className='w-full h-full rounded-2xl bg-center bg-cover duration-500' style={{backgroundImage: `url(${slides[currentIndex].url})`}}>
@@ -45,6 +50,14 @@ const ImageSlider = () => {
         {/*  right arrow */}
         <div className='absolute top-[50%] translate-x-0 translate-y-[-50%] right-5 text-2xl rounded-full p-2 group-hover:bg-black/20 text-white cursor-pointer'>
             <BsChevronCompactRight onClick={nextSlide} size={30} />
+        </div>
+
+        <div className='flex top-4 justify-center py-2'>
+            {slides.map((slide, slideIndex) => (
+               <div className='text-2xl cursor-pointer' key={slideIndex} onClick={() => goToSlide(slideIndex)}>
+                   <RxDotFilled />
+                </div>
+                ))}
         </div>
     </div>
     
