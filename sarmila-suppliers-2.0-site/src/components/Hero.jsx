dbcc712ff54027d1cc6fe06beb8277eeef1cd463
Rohan Maharjan <hondazmr6@gmail.com>
2023-07-26 13:53:21 +0545

    initial commit

--- sarmila-suppliers-2.0-site/src/components/Hero.jsx
+++ sarmila-suppliers-2.0-site/src/components/Hero.jsx
@@ -4,9 +4,9 @@ import Typed from 'react-typed'
 const Hero = () => {
   return (
     <div className='text-white'>
-        <div className='max-w-[800px] mt-[-96px] w-full h-screen mx-auto text-center flex flex-col justify-center'>
-            <p className='text-[#00df9a] uppercase font-bold p-2'>Growing with data analytics</p>
-            <h1 className='md:text-7xl sm:text-6xl text-4xl font-bold md:py-6'>Grow with data</h1>
+        <div className='max-w-[800px] mt-[-96px] w-full h-screen mx-auto text-center flex flex-col justify-center '>
+            <p className='text-[#00df9a] uppercase font-bold p-2'>Best Oxygen Service in Kathmandu</p>
+            <h1 className='md:text-7xl sm:text-6xl text-4xl font-bold md:py-6'>Oxygen Available 24/7</h1>
             <div className='flex justify-center items-center'>
                 <p className='md:tex-5xl sm:text-4xl text-xl font-bold py-4'>Fast, FLexible, Financing for </p>
                 <Typed className='md:tex-5xl sm:text-4xl text-xl font-bold md:pl-4 pl-2' strings={['BTB', 'BTC', 'SAAS']} typeSpeed={120} backSpeed={140} loop />
--- sarmila-suppliers-2.0-site/src/components/Navbar.jsx
+++ sarmila-suppliers-2.0-site/src/components/Navbar.jsx
@@ -1,5 +1,6 @@
 import React, {useState} from 'react'
 import {AiOutlineClose, AiOutlineMenu} from 'react-icons/ai'
+import Logo from "../assets/logo.png"
 
 const Navbar = () => {
     const [nav, setNav] = useState(false)
@@ -9,13 +10,15 @@ const Navbar = () => {
         setNav(!nav)
     }
   return (
-    <div className='flex justify-between items-center h-24 max-w-[1240px] mx-auto px-4 text-white'>
-        <h1 className='w-full text-3xl font-bold text-[#00df9a] cursor-pointer'>REACT.</h1>
+    <div className='flex justify-between items-center h-24 max-w-full mx-auto px-4 text-black bg-white'>
+        
+        <img src={Logo} className='w-[50px] h-[50px] cursor-pointer' alt="" />
+        <h1 className='w-full text-l font-bold text-[#000000] cursor-pointer'>SARMILA SUPPLIERS</h1>
         <ul className='hidden md:flex cursor-pointer'>
             <li className='p-4'>Home</li>
-            <li className='p-4'>Company</li>
-            <li className='p-4'>Resources</li>
             <li className='p-4'>About</li>
+            <li className='p-4'>Service</li>
+            <li className='p-4'>Product</li>
             <li className='p-4'>Contact</li>
         </ul>
         <div onClick={handleNav} className='block md:hidden'>
@@ -23,12 +26,12 @@ const Navbar = () => {
         </div>
         {/* mobile menu */}
         <ul className={nav ? 'fixed left-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-[#000300] ease-in-out duration-500' : 'fixed left-[-100%]'}>
-            <h1 className='w-full text-3xl font-bold text-[#00df9a] m-4'>REACT.</h1>
-                <li className='p-4 border-b border-gray-600'>Home</li>
-                <li className='p-4 border-b border-gray-600'>Company</li>
-                <li className='p-4 border-b border-gray-600'>Resources</li>
-                <li className='p-4 border-b border-gray-600'>About</li>
-                <li className='p-4 border-b border-gray-600'>Contact</li>
+        <img src={Logo} className='ml-4 mt-6 w-[50px] h-[50px] cursor-pointer' alt="" />
+                <li className='p-4 border-b border-gray-600 text-white cursor-pointer'>Home</li>
+                <li className='p-4 border-b border-gray-600 text-white cursor-pointer'>About</li>
+                <li className='p-4 border-b border-gray-600 text-white cursor-pointer'>Service</li>
+                <li className='p-4 border-b border-gray-600 text-white cursor-pointer'>Product</li>
+                <li className='p-4 border-b border-gray-600 text-white cursor-pointer'>Contact</li>
         </ul>
     </div>
   )
