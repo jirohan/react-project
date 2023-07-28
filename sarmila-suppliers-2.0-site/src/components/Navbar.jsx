ccc602fa1b817921143bde1a7b7c31a3f21a2fd2
Rohan Maharjan <hondazmr6@gmail.com>
2023-07-28 15:06:02 +0545

    updates on navbar

--- sarmila-suppliers-2.0-site/src/components/Navbar.jsx
+++ sarmila-suppliers-2.0-site/src/components/Navbar.jsx
@@ -10,33 +10,43 @@ const Navbar = () => {
         setNav(!nav)
     }
   return (
-    <div className='flex justify-between items-center h-24 max-w-full mx-auto px-4 text-black bg-white'>
-        
-        <img src={Logo} className='w-[50px] h-[50px] cursor-pointer' alt="" />
-        <h1 className='w-full text-l font-bold text-[#000000] cursor-pointer'>SARMILA SUPPLIERS</h1>
-        <ul className='hidden md:flex cursor-pointer'>
-            <li className='p-4'>Home</li>
-            <li className='p-4'>About</li>
-            <li className='p-4'>Service</li>
-            <li className='p-4'>Product</li>
-            <li className='p-4'>Contact</li>
-        </ul>
-        <div onClick={handleNav} className='block md:hidden'>
-            {nav ? <AiOutlineClose size={20} className='cursor-pointer'/> : <AiOutlineMenu size={20} className='cursor-pointer'/>}
-        </div>
-        {/* mobile menu */}
-        <ul className={nav ? 'fixed left-0 top-0 w-[70%] h-full border-r border-r-gray-900 bg-[#000300] ease-in-out duration-500' : 'fixed left-[-100%]'}>
-            <div className='flex'>
-                <img src={Logo} className='ml-4 mt-6 w-[50px] h-[50px] cursor-pointer' alt="" />
-                <h1 className='w-full text-l mt-9 mr-4 font-bold text-[#ffffff] cursor-pointer'>SARMILA SUPPLIERS</h1>
+    <div className='w-full bg-white'>
+        <div className='max-w-[1240px] h-[90px] mx-auto px-4 flex justify-between items-center text-black bg-white'>
+            {/* LOGO AND TEXT */}
+            <div className='flex items-center'>
+                <img src={Logo} className='w-[50px] h-[50px] cursor-pointer' alt="" />
+                <h1 className='w-full text-l font-bold text-[#000000] cursor-pointer'>SARMILA SUPPLIERS</h1>
             </div>
-                <li className='p-4 border-b border-gray-600 text-white cursor-pointer'>Home</li>
-                <li className='p-4 border-b border-gray-600 text-white cursor-pointer'>About</li>
-                <li className='p-4 border-b border-gray-600 text-white cursor-pointer'>Service</li>
-                <li className='p-4 border-b border-gray-600 text-white cursor-pointer'>Product</li>
-                <li className='p-4 border-b border-gray-600 text-white cursor-pointer'>Contact</li>
-        </ul>
-    </div>
+            {/* ul and li elements */}
+            <div className='hidden md:flex cursor-pointer'>    
+                <ul className='flex items-center'>
+                    <li className='p-4'>Home</li>
+                    <li className='p-4'>About</li>
+                    <li className='p-4'>Service</li>
+                    <li className='p-4'>Product</li>
+                    <li className='p-4'>Contact</li>
+                </ul>
+            </div>    
+            {/* Hamburger Menu */}
+            <div onClick={handleNav} className='block md:hidden'>
+                {nav ? <AiOutlineClose size={20} className='cursor-pointer'/> : <AiOutlineMenu size={20} className='cursor-pointer'/>}
+            </div>
+            {/* mobile menu */}
+            <div className={nav ? 'w-[70%] h-full fixed left-0 top-0 border-r border-r-gray-900 bg-[#000300] justify-center text-start ease-in-out duration-500' : 'fixed left-[-100%]'}>
+                <div className='flex'>
+                    <img src={Logo} className='ml-4 mt-6 w-[50px] h-[50px] cursor-pointer' alt="" />
+                    <h1 className='w-full text-l mt-9 mr-4 font-bold text-[#ffffff] cursor-pointer'>SARMILA SUPPLIERS</h1>
+                </div>
+                <ul>
+                    <li className='p-4 border-b border-gray-600 text-white cursor-pointer'>Home</li>
+                    <li className='p-4 border-b border-gray-600 text-white cursor-pointer'>About</li>
+                    <li className='p-4 border-b border-gray-600 text-white cursor-pointer'>Service</li>
+                    <li className='p-4 border-b border-gray-600 text-white cursor-pointer'>Product</li>
+                    <li className='p-4 border-b border-gray-600 text-white cursor-pointer'>Contact</li>
+                </ul>
+            </div>    
+        </div>
+    </div>    
   )
 }
 
