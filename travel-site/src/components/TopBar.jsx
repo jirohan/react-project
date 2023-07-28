ebf235998dc723fb8d69beaa6f3330a0976a8391
Rohan Maharjan <hondazmr6@gmail.com>
2023-07-28 17:56:38 +0545

    topbar update

--- travel-site/src/components/TopBar.jsx
+++ travel-site/src/components/TopBar.jsx
@@ -5,13 +5,13 @@ import {BsChatSquareDots} from 'react-icons/bs'
 
 const TopBar = () => {
   return (
-    <div className='flex justify-between items-center px-4 py-4'>
+    <div className='flex justify-between items-center px-4 py-4 bg-blue-50'>
         <div className='flex items-center'>
             <BsChatSquareDots size={30} className='text-[var(--primary-dark)] mr-2'/>
             <h1 className='text-xl font-bold text-gray-700'>MONTHAWAY</h1>
         </div>
         <div className='flex'>
-            <div className='hidden md:flex items-center px-6'>
+            <div className='hidden md:flex items-center px-6 '>
                 <AiOutlineClockCircle size={20} className='mr-2 text-[var(--primary-dark)]' />
                 <p className='text-sm text-gray-800'>9AM - 5PM</p>
             </div>
