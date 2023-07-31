aea54bcd4596f7821dc04842227acc7ead288d32
Rohan Maharjan <hondazmr6@gmail.com>
2023-07-31 23:41:33 +0545

    working in app

---
+++ college website/src/components/Navbar.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='w-full h-[90px] bg-blue-300'>
+        <div className='w=[80%] bg-blue-400'>
+            {/* logo and h1 */}
+            <div className='flex'>
+                <h1>NEPCOLLEGE</h1>
+            </div>
+            {/* ul and li elememt */}
+            <div>
+
+            </div>
+            {/*  hamburger menu */}
+            <div>
+
+            </div>
+            {/*  mobile menu */}
+            <div>
+
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
