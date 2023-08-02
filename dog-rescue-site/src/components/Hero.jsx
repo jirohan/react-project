f3f7e525d7316398bd0d89b44467250b9bc62466
Rohan Maharjan <hondazmr6@gmail.com>
2023-08-02 11:33:53 +0545

    hero component

---
+++ dog-rescue-site/src/components/Hero.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <div>
+        <img src="https://images.unsplash.com/photo-1561037404-61cd46aa615b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8ZG9nfGVufDB8fDB8fHww&auto=format&fit=crop&w=900&q=60" alt="" />
+    </div>
+  )
+}
+
+export default Hero
