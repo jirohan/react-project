8fb06e740c3c68fb8420c6b835c9928fa690df5e
Rohan Maharjan <hondazmr6@gmail.com>
2023-08-02 14:28:22 +0545

    working with nav and hero

--- dog-rescue-site/src/components/Hero.jsx
+++ dog-rescue-site/src/components/Hero.jsx
@@ -2,12 +2,17 @@ import React from 'react'
 
 const Hero = () => {
   return (
-    <div className='w-full h-[90vh]'>
-        <img src="https://images.unsplash.com/photo-1561037404-61cd46aa615b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8ZG9nfGVufDB8fDB8fHww&auto=format&fit=crop&w=900&q=60" alt="" className='w-full h-full object-cover' />
-        <div className='max-w-[1040px] m-auto'>
-            <div className='absolute top-[20%] left-[30%] md:top-[40%] md:left-[10%] w-full max-w-[1040px] h-full flex flex-col text-white '>
-                <h1 className='font-bold text-3xl md:4xl lg:text-6xl'>WE CARE FOR <span className='md:block'>YOUR DOG</span></h1>
-                <h4>We always try to provide your dog the best services.</h4>
+    <div className='w-full h-screen'>
+        <img className='w-full h-screen object-cover' src="https://images.unsplash.com/photo-1561037404-61cd46aa615b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8ZG9nfGVufDB8fDB8fHww&auto=format&fit=crop&w=900&q=60" alt=""  />
+        <div className='w-full m-auto'>
+            <div className='absolute top-0 w-full h-full flex flex-col justify-center text-white'>
+              <div className='left-[20%] top-[20%] md:left-[10%] md:top-[50%] max-w-[1100px] m-auto absolute p-4'>
+                <h1 className='font-bold text-3xl md:text-5xl lg:text-7xl'>
+                WE CARE FOR <span className='md:block'>YOUR DOG</span>
+                </h1>
+                <p>We always try to provide <span className='md:block'>your dog the best services.</span></p>
+                <button className='border py-2 px-3 rounded-full'>Call Now</button>
+              </div>  
             </div>
         </div>
     </div>
--- dog-rescue-site/src/components/Navbar.jsx
+++ dog-rescue-site/src/components/Navbar.jsx
@@ -2,12 +2,32 @@ import React, { useState } from 'react'
 import {AiOutlineMenu, AiOutlineClose, AiOutlineCaretUp, AiOutlineCaretDown} from 'react-icons/ai'
 import list from "../list.json"
 
+const renderLinks = () => {
+    return(
+        <div className='bg-white absolute top-[110px] flex flex-col items-start rounded-xl p-4 w-[200px] cursor-pointer shadow-2xl'>
+                            {list.map((item, i) => (
+                                <div className='flex w-full justify-between hover:bg-gray-50 cursor-pointer rounded-r-xl border-l-transparent p-2 hover:border-l-green-700 border-l-4 ' key={i}>
+                                    <h3>{item.dropdown}</h3>
+                                    <ul>
+                                        {item.links.map((link, j) => (
+                                            <li key={j}>
+                                                <a href={link.url}>{link.title}</a>
+                                            </li>
+                                        ))}
+                                    </ul>
+                                </div>    
+                            ))}
+                        </div>
+    )
+};
+
 const Navbar = () => {
     const [nav, setNav] = useState(false)
     const handleNav = () => {
         setNav(!nav)
     }
     const [isOpen, setIsOpen] = useState(false)
+    
   return (
     <div className='w-full h-[90px] bg-gray-50'>
         <div className='max-w-[1240px] h-full mx-auto px-4 flex justify-between items-center'>
@@ -28,7 +48,7 @@ const Navbar = () => {
                                         <AiOutlineCaretUp className='h-7'  /> ) : ( <AiOutlineCaretDown className='h-7' />)
                                 }
                         </button>
-                            {isOpen && <div className='bg-white absolute top-[90px] flex flex-col items-start rounded-xl p-4 w-[200px] cursor-pointer shadow-2xl'>
+                            {isOpen && <div className='bg-red-50 absolute top-[90px] flex flex-col items-start rounded-xl p-4 w-[200px] cursor-pointer shadow-2xl'>
                                 {list.map((item, i) => (
                                     <div className='flex w-full justify-between hover:bg-gray-50 cursor-pointer rounded-r-xl border-l-transparent p-2 hover:border-l-green-700 border-l-4' key={i}>
                                         <h3>{item.dropdown}</h3>
@@ -46,7 +66,7 @@ const Navbar = () => {
                 {nav ? <AiOutlineClose size={30} className='text-black'/> :<AiOutlineMenu size={30} className='text-black' />}
             </div>
             {/* mobile menu */}
-            <div className={nav ? 'w-[45%] h-[45%] absolute top-[90px] right-0 justify-center bg-white text-black shadow-lg' : 'absolute right-[-0%] top-[-80%]'}>
+            <div className={nav ? 'w-[45%] h-[45%] absolute top-[90px] right-0 justify-center bg-white text-black shadow-lg ' : 'absolute top-0 h-screen left-[-100%] ease-in duration-500'}>
                 <ul className='text-xl'>
                     <li>Home</li>
                     <div className='flex flex-col p-4'>
@@ -58,13 +78,7 @@ const Navbar = () => {
                                         <AiOutlineCaretUp className='h-7'  /> ) : ( <AiOutlineCaretDown className='h-7' />)
                                 }
                             </button>
-                            {isOpen && <div className='bg-white absolute top-[110px] flex flex-col items-start rounded-xl p-4 w-[200px] cursor-pointer shadow-2xl'>
-                                {list.map((item, i) => (
-                                    <div className='flex w-full justify-between hover:bg-gray-50 cursor-pointer rounded-r-xl border-l-transparent p-2 hover:border-l-green-700 border-l-4 ' key={i}>
-                                        <h3>{item.dropdown}</h3>
-                                    </div>    
-                                ))}
-                                </div>}
+                            {isOpen && renderLinks()}
                     </div>
                     <li>Trainers</li>
                     <li>Services</li>
@@ -77,3 +91,4 @@ const Navbar = () => {
 }
 
 export default Navbar
+
