b8235ac7ebfea13abb1fd8b82bdcb4376805cdd8
Rohan Maharjan <hondazmr6@gmail.com>
2023-08-03 14:42:20 +0545

    offer

--- school-website/src/components/Offer.jsx
+++ school-website/src/components/Offer.jsx
@@ -1,21 +1,99 @@
 import React from 'react'
+import {AiOutlineSafetyCertificate} from 'react-icons/ai'
+import {BsBook} from 'react-icons/bs'
 
 const Offer = () => {
   return (
     <div className='max-w-[1240px] mx-auto grid sm:grid-cols-1 lg:grid-cols-3 gap-4 px-4 py-16 '>
-        <div className='lg:col-span-2 flex flex-col justify-evenly'>
-            <div>
-                <h2>What We Offer</h2>
+        <div className='lg:col-span-2 justify-evenly'>
+            <div className='px-4'>
+                <h2 className='text-3xl pb-5'>What We Offer</h2>
                 <p>On her way she met a copy. The copy warned the Little Blind Text, that where it came from it would have been rewritten a thousand times and everything that was left from its origin would be the word.</p>
             </div>
+            <div className='grid md:grid-cols-2 sm:grid-cols-1 py-8'>
+              <div className='grid md:grid-cols-4 sm:grid-cols-2 py-8'>
+                <div className='grid grid-cols-1 mx-auto py-4 bg-blue-400 sm:border sm:border-black md:border-none md:border-transparent md:w-[75px] rounded-full relative px-[20px] justify-center items-center z-20'>
+                  <AiOutlineSafetyCertificate size={40} className='text-white'/>
+                </div>
+                <div className='grid col-span-3'>
+                  <h2 className='text-xl font-bold'>Safety First</h2>
+                  <p>Far far away, behind the word mountains, far from the countries Vokalia.</p>
+                </div>
+              </div>
+              <div className='grid md:grid-cols-4 sm:grid-cols-2 py-8'>
+                <div className='grid grid-cols-1 mx-auto py-4 bg-blue-400 sm:border sm:border-black md:border-none md:border-transparent md:w-[75px]  rounded-full relative px-[20px] justify-center items-center z-20'>
+                  <BsBook size={40} className='text-white'/>
+                </div>
+                <div className='grid col-span-3'>
+                  <h2 className='text-xl font-bold'>Regular Classes</h2>
+                  <p>Far far away, behind the word mountains, far from the countries Vokalia.</p>
+                </div>
+              </div>
+              <div className='grid md:grid-cols-4 sm:grid-cols-2 py-8'>
+                <div className='grid grid-cols-1 mx-auto py-4 bg-blue-400 sm:border sm:border-black md:border-none md:border-transparent md:w-[75px]  rounded-full relative px-[20px] justify-center items-center z-20'>
+                  <AiOutlineSafetyCertificate size={40} className='text-white'/>
+                </div>
+                <div className='grid col-span-3'>
+                  <h2 className='text-xl font-bold'>Safety First</h2>
+                  <p>Far far away, behind the word mountains, far from the countries Vokalia.</p>
+                </div>
+              </div>
+              <div className='grid md:grid-cols-4 sm:grid-cols-2 py-8'>
+                <div className='grid grid-cols-1 mx-auto py-4 bg-blue-400 sm:border sm:border-black md:border-none md:border-transparent md:w-[75px]  rounded-full relative px-[20px] justify-center items-center z-20'>
+                  <AiOutlineSafetyCertificate size={40} className='text-white'/>
+                </div>
+                <div className='grid col-span-3'>
+                  <h2 className='text-xl font-bold'>Safety First</h2>
+                  <p>Far far away, behind the word mountains, far from the countries Vokalia.</p>
+                </div>
+              </div>
+              <div className='grid md:grid-cols-4 sm:grid-cols-2 py-8'>
+                <div className='grid grid-cols-1 mx-auto py-4 bg-blue-400 sm:border sm:border-black md:border-none md:border-transparent md:w-[75px]  rounded-full relative px-[20px] justify-center items-center z-20'>
+                  <AiOutlineSafetyCertificate size={40} className='text-white'/>
+                </div>
+                <div className='grid col-span-3'>
+                  <h2 className='text-xl font-bold'>Safety First</h2>
+                  <p>Far far away, behind the word mountains, far from the countries Vokalia.</p>
+                </div>
+              </div>
+              <div className='grid md:grid-cols-4 sm:grid-cols-2 py-8'>
+                <div className='grid grid-cols-1 mx-auto py-4 bg-blue-400 sm:border sm:border-black md:border-none md:border-transparent md:w-[75px]  rounded-full relative px-[20px] justify-center items-center z-20'>
+                  <AiOutlineSafetyCertificate size={40} className='text-white'/>
+                </div>
+                <div className='grid col-span-3'>
+                  <h2 className='text-xl font-bold'>Safety First</h2>
+                  <p>Far far away, behind the word mountains, far from the countries Vokalia.</p>
+                </div>
+              </div>
+              <div className='grid md:grid-cols-4 sm:grid-cols-2 py-8'>
+                <div className='grid grid-cols-1 mx-auto py-4 bg-blue-400 sm:border sm:border-black md:border-none md:border-transparent md:w-[75px]  rounded-full relative px-[20px] justify-center items-center z-20'>
+                  <AiOutlineSafetyCertificate size={40} className='text-white'/>
+                </div>
+                <div className='grid col-span-3'>
+                  <h2 className='text-xl font-bold'>Safety First</h2>
+                  <p>Far far away, behind the word mountains, far from the countries Vokalia.</p>
+                </div>
+              </div>
+              <div className='grid md:grid-cols-4 sm:grid-cols-2 py-8'>
+                <div className='grid grid-cols-1 mx-auto py-4 bg-blue-400 sm:border sm:border-black md:border-none md:border-transparent md:w-[75px]  rounded-full relative px-[20px] justify-center items-center z-20'>
+                  <AiOutlineSafetyCertificate size={40} className='text-white'/>
+                </div>
+                <div className='grid col-span-3'>
+                  <h2 className='text-xl font-bold'>Safety First</h2>
+                  <p>Far far away, behind the word mountains, far from the countries Vokalia.</p>
+                </div>
+              </div>
+            </div>  
         </div>
-        <div className='lg:col-span-1 flex flex-col-reverse justify-evenly bg-slate-200 px-4 py-4'>
-            <div>
-              <h2>Welcome to Kiddos Learning School</h2>
-              <p>On her way she met a copy. The copy warned the Little Blind Text, that where it came from it would have been rewritten a thousand times and everything that was left from its origin would be the word.
-
-Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean. A small river named Duden flows by their place and supplies it with the necessary regelialia. And if she hasn’t been rewritten, then they are still using her.</p>
-              <button>Learn More</button>
+        <div className='lg:col-span-1 justify-evenly bg-slate-200 px-4 py-4'>
+            <div className='px-4'>
+              <h2 className='text-3xl pb-5'>Welcome to Kiddos Learning School</h2>
+              <p className='text-md'>On her way she met a copy. The copy warned the Little Blind Text, that where it came from it would have been rewritten a thousand times and everything that was left from its origin would be the word.
+              </p><br />
+              <p className='pb-5 text-md'>
+Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean. A small river named Duden flows by their place and supplies it with the necessary regelialia. And if she hasn’t been rewritten, then they are still using her.
+              </p>
+              <button className='px-4 py-3 rounded-full text-white bg-orange-400 hover:bg-orange-500'>Learn More</button>
             </div>
         </div>
     </div>
