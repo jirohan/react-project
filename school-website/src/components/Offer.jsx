89d80b4f1d931b511007f1b375e400d9c9192aea
Rohan Maharjan <hondazmr6@gmail.com>
2023-08-03 11:50:53 +0545

    offer working

--- school-website/src/components/Offer.jsx
+++ school-website/src/components/Offer.jsx
@@ -2,15 +2,21 @@ import React from 'react'
 
 const Offer = () => {
   return (
-    <div className='max-w-[1240px] mx-auto grid lg:grid-cols-3 gap-4 px-4 py-16'>
+    <div className='max-w-[1240px] mx-auto grid sm:grid-cols-1 lg:grid-cols-3 gap-4 px-4 py-16 '>
         <div className='lg:col-span-2 flex flex-col justify-evenly'>
             <div>
                 <h2>What We Offer</h2>
                 <p>On her way she met a copy. The copy warned the Little Blind Text, that where it came from it would have been rewritten a thousand times and everything that was left from its origin would be the word.</p>
             </div>
         </div>
-        <div>
-            right
+        <div className='lg:col-span-1 flex flex-col-reverse justify-evenly bg-slate-200 px-4 py-4'>
+            <div>
+              <h2>Welcome to Kiddos Learning School</h2>
+              <p>On her way she met a copy. The copy warned the Little Blind Text, that where it came from it would have been rewritten a thousand times and everything that was left from its origin would be the word.
+
+Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean. A small river named Duden flows by their place and supplies it with the necessary regelialia. And if she hasn’t been rewritten, then they are still using her.</p>
+              <button>Learn More</button>
+            </div>
         </div>
     </div>
   )
